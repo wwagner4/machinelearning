5731097c9a055651c2a03328e59aa3aaa107293d
wwagner4 <wwagner4@gmail.com>
2016-12-15 20:56:12 +0100

    gradient decent ready for experiments

--- scala-machinelearning/src/test/scala/machinelearning/LinearRegressionSuite.scala
+++ scala-machinelearning/src/test/scala/machinelearning/LinearRegressionSuite.scala
@@ -15,7 +15,7 @@ class LinearRegressionSuite extends FunSuite {
 
   def costFunc(a: Double, b: Double)(trainingSet: List[(Double, Double)]): Double = {
     val m = trainingSet.size
-    val hf = hypothesisFunc(a, b)_
+    val hf = hypothesisFunc(a, b) _
     val s = trainingSet
       .map { case (x, y) => hf(x) - y }
       .map(math.pow(_, 2))
@@ -28,12 +28,15 @@ class LinearRegressionSuite extends FunSuite {
       (0.0, 0.0),
       (1.0, 0.0),
       (0.0, 1.0),
-      (1.0, 1.0))
+      (1.0, 1.0),
+      (1.93, 0.82),
+      (1.94, 0.82))
 
-    parameters.foreach{case (a, b) =>
-      val cf = costFunc(a, b)_
+    println("-- COST FUNCTION -----------------------------")
+    parameters.foreach { case (a, b) =>
+      val cf = costFunc(a, b) _
       val cost = cf(trainingSet)
-      println("%.2f, %.2f -> %.2f" format(a, b, cost))
+      println("  (%5.2f, %5.2f) -> %5.5f" format(a, b, cost))
 
     }
 
@@ -41,7 +44,7 @@ class LinearRegressionSuite extends FunSuite {
 
   def dfACost(a: Double, b: Double)(trainingSet: List[(Double, Double)]): Double = {
     val m = trainingSet.size
-    val hf = hypothesisFunc(a, b)_
+    val hf = hypothesisFunc(a, b) _
     val s = trainingSet
       .map { case (x, y) => hf(x) - y }
       .sum
@@ -50,23 +53,27 @@ class LinearRegressionSuite extends FunSuite {
 
   def dfBCost(a: Double, b: Double)(trainingSet: List[(Double, Double)]): Double = {
     val m = trainingSet.size
-    val hf = hypothesisFunc(a, b)_
+    val hf = hypothesisFunc(a, b) _
     val s = trainingSet
       .map { case (x, y) => (hf(x) - y) * x }
       .sum
     s / m
   }
 
-  def oneStep(a: Double, b:Double): (Double, Double) = {
-    val alpha = 0.1
-    ???
-
+  def oneStep(alpha: Double)(trainingSet: List[(Double, Double)])(a: Double, b: Double): (Double, Double) = {
+    val a1 = a - alpha * dfACost(a, b)(trainingSet)
+    val b1 = b - alpha * dfBCost(a, b)(trainingSet)
+    (a1, b1)
   }
 
   test("Gradient decent") {
-
-
-
+    println("-- GRADIENT DECENT -----------------------------")
+    val fs = oneStep(0.1)(trainingSet) _
+    val steps = Stream.iterate((0.0, 0.0)) { case (a, b) => fs(a, b) }
+    steps.take(100)
+      .zipWithIndex
+      .filter { case (_, i) => i % 10 == 0 }
+      .foreach { case ((a, b), i) => println("%4d: %6.2f %6.2f" format(i, a, b)) }
   }
 
 }
