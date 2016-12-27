1b2766e0ebdb93aa684d9657eb2ee456d7b8f061
wwagner4 <wwagner4@gmail.com>
2016-12-27 10:30:19 +0100

    vectorised gradient descent works fine

--- scala-machinelearning/src/main/scala/machinelearning/GradientDescent.scala
+++ scala-machinelearning/src/main/scala/machinelearning/GradientDescent.scala
@@ -15,16 +15,16 @@ object GradientDescent {
   /**
     * A function with two input vectors an one output value
     *
-    * thet => X => y
+    * theta => X => y
     *
-    * thet: column vector of parameters (to be optimized)
-    * X   : sample values (featuers). first column must be 1.0
-    * y   : result (column vector) of the hypothesis (to be minimized)
+    * theta: column vector of parameters (to be optimized)
+    * X    : sample values (features). first column must be 1.0
+    * y    : result (column vector) of the hypothesis (to be minimized)
     */
   type HypType = DenseMatrix[Double] => DenseMatrix[Double] => DenseMatrix[Double]
 
-  def costFunc(hyp: HypType)(ts: TrainingSet)(thet: DenseMatrix[Double]): Double = {
-    val h = hyp(thet)(_)
+  def costFunc(hyp: HypType)(ts: TrainingSet)(theta: DenseMatrix[Double]): Double = {
+    val h = hyp(theta)(_)
     val m = ts.y.rows
     sum((h(ts.X) - ts.y) :^ 2.0) / (2 * m)
   }
@@ -33,20 +33,15 @@ object GradientDescent {
     * @param alpha step width
     * @param hypo hypothesis function
     * @param ts training set
-    * @param thet set of parameters
+    * @param theta set of parameters
     * @return the next set of parameters
     */
   def gradientDescent(alpha: Double)(hypo: HypType)(ts: TrainingSet)
-                     (thet: DenseMatrix[Double]): DenseMatrix[Double] = {
+                     (theta: DenseMatrix[Double]): DenseMatrix[Double] = {
     val m = ts.y.rows.toDouble
-    val hf = hypo(thet)(_)
+    val hf = hypo(theta)(_)
 
-    val v0 = hf(ts.X)
-    println(s"v0 ${v0.rows} x ${v0.cols}\n$v0")
-    println(s"ts.y ${ts.y.rows} x ${ts.y.cols}\n${ts.y}")
-    val v1 = v0 - ts.y
-
-    thet - alpha * ((1/m) *  v1.t * ts.X).t
+    theta - alpha * ((1/m) * (hf(ts.X) - ts.y).t * ts.X).t
   }
 
 }
@@ -57,13 +52,13 @@ object HypothesisFunction {
     * Linear function of type 'HypType'
     */
   def linearFunc: GradientDescent.HypType = {
-    case (thet: DenseMatrix[Double]) => (X: DenseMatrix[Double]) => {
-      require(thet.cols == 1)
-      require(thet.rows > 0)
+    case (theta: DenseMatrix[Double]) => (X: DenseMatrix[Double]) => {
+      require(theta.cols == 1)
+      require(theta.rows > 0)
       require(X.rows > 0)
-      require(X.cols == thet.rows)
+      require(X.cols == theta.rows)
 
-      X * thet
+      X * theta
 
     }
   }
--- scala-machinelearning/src/test/scala/machinelearning/MultivariantLinearRegressionSuite.scala
+++ scala-machinelearning/src/test/scala/machinelearning/MultivariantLinearRegressionSuite.scala
@@ -85,16 +85,16 @@ class MultivariantLinearRegressionSuite extends FunSuite {
 
   {
     val expectedValues = Map(
-      0 -> DenseMatrix(0.00, 0.00).t,
-      10 -> DenseMatrix(0.99, 1.12).t,
-      20 -> DenseMatrix(1.35, 1.01).t,
-      30 -> DenseMatrix(1.57, 0.94).t,
-      40 -> DenseMatrix(1.71, 0.89).t,
-      50 -> DenseMatrix(1.80, 0.87).t,
-      60 -> DenseMatrix(1.85, 0.85).t,
-      70 -> DenseMatrix(1.88, 0.84).t,
-      80 -> DenseMatrix(1.91, 0.83).t,
-      90 -> DenseMatrix(1.92, 0.83).t)
+      0 -> DenseMatrix(0.00, 0.00),
+      10 -> DenseMatrix(0.99, 1.12),
+      20 -> DenseMatrix(1.35, 1.01),
+      30 -> DenseMatrix(1.57, 0.94),
+      40 -> DenseMatrix(1.71, 0.89),
+      50 -> DenseMatrix(1.80, 0.87),
+      60 -> DenseMatrix(1.85, 0.85),
+      70 -> DenseMatrix(1.88, 0.84),
+      80 -> DenseMatrix(1.91, 0.83),
+      90 -> DenseMatrix(1.92, 0.83))
     val gd = gradientDescent(0.1)(linearFunc)(ts01) _
     val initialThet = DenseMatrix(0.0, 0.0)
     val steps = Stream.iterate(initialThet)(thet => gd(thet))
@@ -110,16 +110,16 @@ class MultivariantLinearRegressionSuite extends FunSuite {
 
   {
     val expectedValues = Map(
-      0 -> DenseMatrix(0.00, 0.00, 0.00, 0.00).t,
-      50 -> DenseMatrix(1.00, 0.77, -0.11, 0.25).t,
-      100 -> DenseMatrix(1.49, 0.83, -0.19, 0.10).t,
-      150 -> DenseMatrix(1.76, 0.85, -0.23, 0.03).t,
-      200 -> DenseMatrix(1.91, 0.85, -0.25, -0.00).t,
-      250 -> DenseMatrix(2.00, 0.85, -0.26, -0.02).t,
-      300 -> DenseMatrix(2.05, 0.85, -0.27, -0.02).t,
-      350 -> DenseMatrix(2.07, 0.85, -0.27, -0.03).t,
-      400 -> DenseMatrix(2.09, 0.84, -0.27, -0.03).t,
-      450 -> DenseMatrix(2.10, 0.84, -0.27, -0.03).t)
+      0 -> DenseMatrix(0.00, 0.00, 0.00, 0.00),
+      50 -> DenseMatrix(1.00, 0.77, -0.11, 0.25),
+      100 -> DenseMatrix(1.49, 0.83, -0.19, 0.10),
+      150 -> DenseMatrix(1.76, 0.85, -0.23, 0.03),
+      200 -> DenseMatrix(1.91, 0.85, -0.25, -0.00),
+      250 -> DenseMatrix(2.00, 0.85, -0.26, -0.02),
+      300 -> DenseMatrix(2.05, 0.85, -0.27, -0.02),
+      350 -> DenseMatrix(2.07, 0.85, -0.27, -0.03),
+      400 -> DenseMatrix(2.09, 0.84, -0.27, -0.03),
+      450 -> DenseMatrix(2.10, 0.84, -0.27, -0.03))
 
     val gd = gradientDescent(0.05)(linearFunc)(ts02) _
     val initialThet = DenseMatrix(0.0, 0.0, 0.0, 0.0)
@@ -138,9 +138,10 @@ class MultivariantLinearRegressionSuite extends FunSuite {
     * Compares two double vectors
     */
   private def eq(is: DenseMatrix[Double], should: DenseMatrix[Double]): Unit = {
-    is.size should be(should.size)
+    is.rows should be(should.rows)
+    is.cols should be(should.cols)
     for (i <- 0 until is.rows; j <- 0 until is.cols) {
-      is(i, j) should be(should(i, i) +- 0.05)
+      is(i, j) should be(should(i, j) +- 0.05)
     }
 
   }
